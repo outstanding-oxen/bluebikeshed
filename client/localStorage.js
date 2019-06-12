a67dc23ab061e586763eecf272e57fcb0478118d
Jason Cho <jason.cho.86@gmail.com>
2019-06-12 16:29:16 -0400

    created methods to load and save state in localStorage.js

---
+++ client/localStorage.js
@@ -0,0 +1,25 @@
+export const loadState = () => {
+  // Need a try/catch block because localStorage.getItem() can fail if privacy
+  // mode does not allow use of localStorage
+  try {
+    // 1. Pull state from localStorage object
+    // 2. If the state is null, return undefined, otherwise return state
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (err) {
+    return undefined
+  }
+}
+
+export const saveState = state => {
+  try {
+    // Need to stringify state because localStorage will only allow string values
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch {
+    // ignore write errors
+  }
+}
