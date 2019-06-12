7c917e31d39770f22dd60b969059cbcc52f68cf6
Ruslan Zyabbarov <rupam71@gmail.com>
2019-06-11 23:10:58 -0400

    Navbar, Loading Indicator, SignUp and SignIn Forms, basic react routes. Also favicon changed to Big O as Oxen. Loupe icon added un search field

--- client/app.js
+++ client/app.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
-
+import Loading from './components/Loading'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      {/* <Loading /> */}
     </div>
   )
 }
