c3402f2f6afb8510b6ba6f8c8f6a4dbdd4eb902f
Sri Velagapudi <srivar1997@gmail.com>
2019-06-17 16:24:38 -0400

    added post route for order detail

--- server/api/orderdetails.js
+++ server/api/orderdetails.js
@@ -4,7 +4,7 @@ const OrderDetail = require('../db/models/orderDetail')
 // post order given a specific user
 router.post('/:id', (req, res, next) => {
   let neworder = req.body
-  neworder.userId = req.params.id
+  neworder.orderId = req.params.id
   return OrderDetail.create(neworder)
     .then(order => res.json(order))
     .catch(next)
